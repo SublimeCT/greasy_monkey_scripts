c3db1b2d05d305fb5bb1a7352ba52fd0ec804db4
SublimeCT <hellosc@qq.com>
2017-10-16 21:45:09 +0800

    完成行为类, 将行为和按键类分离

--- baidu_key_tool.js
+++ baidu_key_tool.js
@@ -23,10 +23,115 @@
     'use strict';
 
     /**
+     * 快捷键行为统一接口
+     * @create V0.0.4
+     */
+    class Baidu_Key_Tool_Action {
+        /**
+         * 设置默认属性值
+         * @param Object attr 
+         */
+        constructor () {
+            this.state = true
+            this.switchKeyCode = null
+            this.params = null
+        }
+        /**
+         * 抛出行为参数错误
+         * @param Exception param 
+         */
+        _paramError (param) {
+            throw new Error('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n Action Param Error \n' + param + ' is error \n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n')
+        }
+
+        /**
+         * 属性 & 方法
+         */
+        // 是否开启该行为
+        get state () { return this.state }
+        set state (v) {
+            if (typeof v !== 'boolean') {
+                this._paramError('state')
+            }
+            return v
+        }
+        // 切换开启状态键
+        get switchKeyCode () { return this.switchKeyCode }        
+        set switchKeyCode (v) {
+            if (typeof v !== 'number' && v !== null) {
+                this._paramError('switchKeyCode')
+            }
+            return v
+        }
+        // 自定义参数
+        get params () { return this.params }        
+        set params (v) {
+            if (typeof v !== 'object' && v !== null) {
+                this._paramError('params')
+            }
+            return v
+        }
+        /**
+         * 键值
+         * @note  当值为 [-1] 时为任意键触发
+         */
+        get keyCodes () {
+            if (typeof this.keyCodes === 'undefined') {
+                throw new Error('shit man')
+            }
+            return this.keyCodes
+        }        
+        set keyCodes (v) {
+            if (v.__proto__.constructor !== Array || v.length <1) {
+                this._paramError('keyCodes')
+            }
+            return v
+        }
+        /**
+         * keyup / keydown 必须实现其中一个
+         */
+        downEvent () {
+            if (!this.upEvent) {
+                throw new Error('downEvent is must set')
+            }
+        }
+        set downEvent (v) {
+            if (typeof v !== 'function') {
+                this._paramError('downEvent')
+            }
+        }
+        upEvent () {
+            if (!this.downEvent) {
+                throw new Error('upEvent is must set')                
+            }
+        }
+        set upEvent (v) {
+            if (typeof v !== 'function') {
+                this._paramError('upEvent')
+            }
+        }
+    }
+
+    /**
      * 快捷键工具对象
      * @type Object
      */
     const Baidu_Key_Tool = {
+        /**
+         * DOM 元素 selecttor
+         */
+        selector: {
+            // 输入框
+            input: '#kw'
+        },
+        /**
+         * 已按下的键
+         */
+        pressed: [],
+        /**
+         * 是否正在输入
+         */
+        isInputing: false,
         /**
          * 此处问题较多, 后期将统一配置和行为对象接口
          */
@@ -36,43 +141,10 @@
             // 自定义行为中的参数 | array>object>{string: object}
             setAction: []
         },
-        actions: {
-            index: {
-                // 默认是否开启
-                state: true,
-                // 切换开启状态键
-                switchKeyCode: null,
-                // 键值
-                keyCodes: [97, 98, 99, 100, 101, 102, 103, 104, 105, 96],
-                // 自定义参数
-                params: {},
-                // 事件函数
-                event ({key: {index, keyCode}}) {
-                    document.getElementById(index+1).querySelector('a').click()
-                }
-            },
-            search: {
-                state: true,
-                switchKeyCode: null,
-                keyCodes: [110],
-                params: {},
-                event ({key: {keyCode}}) {
-                    setTimeout(() => {
-                        document.getElementById('kw').focus()
-                        document.getElementById('kw').select()
-                    }, 77)
-                }
-            },
-            showKeyCode: {
-                state: false,
-                switchKeyCode: null,
-                keyCodes: [],
-                params: {},
-                event ({key: {keyCode}, event}) {
-                    console.log('test')
-                }
-            }
-        },
+        /**
+         * 行为
+         */
+        actions: {},
         /**
          * 暂时用 setInterval 实现, 有时间再看 MutationObserver ...
          *    将第一条搜索结果 DOM 元素保存, 不断对比
@@ -89,14 +161,31 @@
                 }, 1300)
             }
         },
-        init (options = null) {
+        /**
+         * 初始化方法
+         * @param object options 
+         */
+        init ({options = null, actions = null}) {
             // 合并 options
             if (options) this.options = Object.assign(this.options, options)
-
             // 动态监听页面变化
             this.showIndex().listenDom.init(this)
-
+            // 监听是否正在输入
+            this._listenInputing()
+            // 注册行为
+            this.actions = actions
             return this
+        },
+        /**
+         * 监听用户是否在搜索框内输入内容
+         */
+        _listenInputing () {
+            document.querySelector(this.selector.input).addEventListener('focus', () => {
+                this.isInputing = true
+            })
+            document.querySelector(this.selector.input).addEventListener('blur', () => {
+                this.isInputing = false
+            })
         },
         /**
          * 显示 numer
@@ -104,8 +193,8 @@
          */
         showIndex () {
             for (let i=0; i<10; i++) {
-                const item = document.getElementById(i+1)
                 try {
+                    const item = document.getElementById(i+1)
                     const locationDom = item.querySelector('h3')
                     const indexNode = this._getIndexNode(i+1)
                     locationDom.style.position = 'relative'
@@ -121,46 +210,48 @@
          * @return object this
          */
         listening () {
-
             document.addEventListener('keydown', event => {
+                // 如果此时正在输入, 则不触发行为
+                if (this.isInputing) return
                 const keyCode = event.keyCode
-                // 执行
+                this.pressed.push(keyCode)
                 // 遍历所有按键行为
                 for (let i in this.actions) {
                     const action = this.actions[i]
                     // 判断是否为模式开关
                     this._switchAction(keyCode, action)
-
                     // 检测该行为是否开启
                     if (!action.state) continue
-
                     // 检测是否触发行为
-                    this._checkKeyCode(event, action)
+                    this._checkKeyCode(event, action, 'downEvent')
+                }
+            })
+            document.addEventListener('keyup', event => {
+                // 如果此时正在输入, 则不触发行为
+                if (this.isInputing) return
+                const keyCode = event.keyCode
+                this.pressed = this.pressed.filter(v => v!=keyCode)
+                // 遍历所有按键行为
+                for (let i in this.actions) {
+                    const action = this.actions[i]
+                    // 检测该行为是否开启
+                    if (!action.state) continue
+                    // 检测是否触发行为
+                    this._checkKeyCode(event, action, 'upEvent')
                 }
             })
             return this
         },
-        _checkKeyCode (event, action) {
+        _checkKeyCode (event, action, eventType) {
             const keyCode = event.keyCode
             // 判断该行为是否是任意键触发
-            if (action.keyCodes.length == 0) {
-                action.event({
-                    event,
-                    key: {
-                        keyCode
-                    }
-                })
+            if (action.keyCodes.length === 1 && actions.keyCode[0] === -1) {
+                action.event({event, key:{keyCode}, pressed: this.pressed})
             } else {
-                // 遍历监听的按键[默认可重复执行多个行为]
+                // 遍历监听的按键
                 for (let [index, code] of action.keyCodes.entries()) {
-                    if (keyCode === code) {
-                        action.event({
-                            event,
-                            key: {
-                                index,
-                                keyCode
-                            }
-                        })
+                    if (keyCode === code && action[eventType]) {
+                        action[eventType]({event, key:{index,keyCode, pressed: this.pressed}})
                     }
                 }
             }
@@ -196,7 +287,41 @@
         }
     }
 
+    // 创建行为
+    /**
+     * 按序号跳转搜索结果
+     */
+    class Actions extends Baidu_Key_Tool_Action {
+        constructor () {
+            super()
+            this.keyCodes = [97, 98, 99, 100, 101, 102, 103, 104, 105, 96]
+        }
+        downEvent ({key: {index, keyCode}, pressed}) {
+            if (pressed.length === 1) {
+                document.getElementById(index+1).querySelector('a').click()        
+            }
+        }
+    }
+
+    /**
+     * 定位搜索框
+     */
+    class Search extends Baidu_Key_Tool_Action {
+        constructor () {
+            super()
+            this.keyCode = [110]
+        }
+        downEvent ({key: {keyCode}, pressed}) {
+            if (pressed.length === 1) {
+                setTimeout(() => {
+                    document.getElementById('kw').focus()
+                    document.getElementById('kw').select()
+                }, 77)
+            }
+        }
+    }
+
     Baidu_Key_Tool
-        .init()
+        .init({}, {search: new Search(), actions: new Actions})
         .listening()
 })();
