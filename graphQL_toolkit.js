ea93ff2b7ce4c9d887bc2cd3c0ffaff3da568a2f
sven <hellosc@qq.com>
2020-11-24 13:43:25 +0800

    feat: 增加 graphQL 脚本

--- graphQL_toolkit.js
+++ graphQL_toolkit.js
@@ -3,15 +3,71 @@
         constructor() { }
         isActive = true
     }
+    class Store {
+        static getOptions() {
+            const options = localStorage.getItem('GraphQLToolkit_options')
+            if (!options) return {}
+            try {
+                return JSON.parse(options) || {}
+            } catch (err) {
+                console.log(err)
+                return {}
+            }
+        }
+        static setOption(options) {
+            localStorage.setItem('GraphQLToolkit_options', JSON.stringify(options))
+        }
+        static updateOptions(options) {
+            const allOptions = Store.getOptions()
+            Object.assign(allOptions, options)
+            Store.setOption(allOptions)
+        }
+    }
+    /**
+     * 使用上次的搜索内容
+     */
+    class QueryToolkitModule extends ToolkitModule {
+        init(ctx) {}
+        static async addEventListeners() {
+            let items
+            for (let times = 20; times--;) {
+                await Toolkit.delay()
+                items = document.querySelectorAll('.graphiql-wrapper > div > div')
+                if (items && items.length) break
+            }
+            if (items.length < 2) throw new Error('missing sidebar element')
+            const sidebar = items[1]
+            const buttons = sidebar.querySelectorAll('div:nth-child(1) > div')
+            buttons[0].click()
+            const apiDoms = items[items.length - 1]
+            let searchInput
+            for (let times = 30; times--;) {
+                await Toolkit.delay()
+                searchInput = apiDoms.querySelector('input[type="text"]')
+                if (searchInput) break
+            }
+            // 直接使用上次搜索内容
+            const options = Store.getOptions()
+            if (options.query) {
+                searchInput.value = options.query // 填充值
+                d.setState({ searchValue: options.query }) // 直接搜索
+            }
+            // 绑定 input 事件记录输入值
+            searchInput.addEventListener('input', evt => {
+                Store.setOption({ query: evt.target.value })
+            })
+            console.log(searchInput, apiDoms)
+        }
+        onload() {
+            QueryToolkitModule.addEventListeners()
+        }
+    }
     /**
      * 加入自定义样式
      */
     class SheetsToolkitModule extends ToolkitModule {
         static _getSheets() {
             return `
-                html {
-                    --document-filter: grayscale(0); /* #html 防止网页被黑白处理, 适用于特殊日期 */
-                }
                 /* 布局样式 */
                 .graphiql-wrapper > div > div:last-of-type {
                     right: auto;
@@ -21,8 +77,8 @@
             `
         }
         init(ctx) {
-            ctx.log('加入自定义样式')
-            SheetsToolkitModule.appendSheets()
+            // ctx.log('加入自定义样式')
+            // SheetsToolkitModule.appendSheets()
         }
         // 通过注入 css 实现隐藏广告并固定布局
         static appendSheets() {
@@ -32,7 +88,9 @@
             el.appendChild(sheet)
             document.getElementsByTagName('head')[0].appendChild(el)
         }
-        onload() {
+        onload(ctx) {
+            ctx.log('加入自定义样式')
+            SheetsToolkitModule.appendSheets()
             // console.error('onload ????????????????')
         }
     }
@@ -45,7 +103,8 @@
         users = {}
         constructor(options = {}) {
             Object.assign(this.options, options)
-            this.emitHook('init')
+            // 禁用 init, 因为必须要等到 onload 才能确定是否是 GraphQL 页面
+            // this.emitHook('init')
         }
         /**
          * 工具集
@@ -75,6 +134,15 @@
         }
     }
     Toolkit.use(new SheetsToolkitModule())
+    Toolkit.use(new QueryToolkitModule())
     window._$GraphQLToolkit = new Toolkit()
-    window.addEventListener('DOMContentLoaded', () => window._$GraphQLToolkit.emitHook('onload'))
+    window.addEventListener('DOMContentLoaded', async () => {
+        for (let times = 10; times--;) {
+            await Toolkit.delay()
+            if (GraphQLPlayground) break
+        }
+        if (!GraphQLPlayground) return
+        // 执行所有模块的钩子函数
+        window._$GraphQLToolkit.emitHook('onload')
+    })
 })();
