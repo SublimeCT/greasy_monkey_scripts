a8264fadf499e78ebb42df42ad55d1f2911d5e2f
sven <hellosc@qq.com>
2023-05-22 23:34:15 +0800

    chore(csdn): 更新版本号

--- csdn_rewrite.js
+++ csdn_rewrite.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSDN 去广告沉浸阅读模式
 // @namespace    http://tampermonkey.net/
-// @version      3.0.6
+// @version      3.0.7
 // @license      GPL-3.0
 // @description  沉浸式阅读 🌈 使用随机背景图片 🎬 重构页面布局 🎯 净化剪切板 🎨 屏蔽一切影响阅读的元素 🎧
 // @description  背景图片取自 https://www.baidu.com/home/skin/data/skin
